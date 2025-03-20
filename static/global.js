036373e0b97268c9694808be199a703a06f569e4
Vitor do Nascimento <vitornascimento@proton.me>
2025-03-20 13:10:08 -0300

    fix route handling

--- static/global.js
+++ static/global.js
@@ -28,9 +28,9 @@ const ARE_WE_HOME = document.documentElement.classList.contains("home");
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    if (!ARE_WE_HOME && !url.startsWith("http")) {
-        url = "../" + url;
-    }
+    // if (!ARE_WE_HOME && !url.startsWith("http")) {
+    //     url = "../" + url;
+    // }
     // nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
 
     let a = document.createElement("a");
