a747368ea0729db536e57fcfae645bc5c7d08d84
Vitor do Nascimento <vitornascimento@proton.me>
2025-03-28 17:10:29 -0300

    projects page enhancements

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,57 +2,45 @@
     <title>My Projects</title>
 </svelte:head>
 
+<h1>
+Projects
+</h1>
+
+<script>
+    import { onMount } from "svelte";
+    import { base } from "$app/paths";
+
+
+    let projects = [];
+
+    onMount(async () => {
+        const response = await fetch(`${base}/projects.json`);
+        projects = await response.json();
+    });
+</script>
+
 <div class="projects">
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Lorem ipsum dolor sit.</h5>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nesciunt reiciendis possimus dicta doloremque amet sunt aspernatur deserunt sequi! Est hic nesciunt libero natus modi earum maiores aut cum incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nesciunt reiciendis possimus dicta doloremque amet sunt aspernatur deserunt sequi! Est hic nesciunt libero natus modi earum maiores aut cum incidunt.</p>
-    </article>
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Voluptatibus sint eos consequuntur.</h5>
-        <p>Quasi et culpa suscipit, alias quaerat fuga natus eos nulla cupiditate cumque eum voluptatem repellendus dignissimos aut fugiat harum ducimus quibusdam corporis, molestiae expedita ullam hic. Debitis in fugiat nam?</p>
-    </article>
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Ab, tenetur aperiam! Id?</h5>
-        <p>Cupiditate quibusdam repellat excepturi facilis aperiam, illum alias architecto aspernatur culpa in a mollitia non laboriosam, odio veritatis totam eum et tenetur quidem nemo inventore. Eos sint quia similique? Similique?</p>
-    </article>
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Omnis perferendis doloribus qui!</h5>
-        <p>Blanditiis eos perferendis, cupiditate accusantium vero adipisci itaque, animi eius voluptatum dolore sapiente ipsum omnis vel in cum nesciunt repellendus a error! Vero ducimus amet fugiat praesentium. Vero, asperiores numquam.</p>
-    </article>
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Omnis totam assumenda maiores!</h5>
-        <p>Error ullam laboriosam nostrum eos soluta mollitia reprehenderit nulla eaque, cumque et voluptates, voluptatibus adipisci, quod eligendi dolorem asperiores tempore laborum laudantium cupiditate provident cum! Possimus ex similique odio autem?</p>
-    </article>
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Qui consequuntur dolore quis?</h5>
-        <p>Tempore, culpa consequatur voluptatem quam, repellat inventore alias nam ut incidunt obcaecati qui neque eaque illum nostrum totam ipsa quae quaerat sint quis reiciendis omnis natus placeat quibusdam? Ut, assumenda.</p>
-    </article>
-    <article>
-        <img src=".././imgs/mm-balloon.jpg" alt="">
-        <h5>Qui consequuntur dolore quis?</h5>
-        <p>Tempore, culpa consequatur voluptatem quam, repellat inventore alias nam ut incidunt obcaecati qui neque eaque illum nostrum totam ipsa quae quaerat sint quis reiciendis omnis natus placeat quibusdam? Ut, assumenda.</p>
-    </article>
+    {#each projects as project}
+        <article>
+            <img src={base}{project.image} alt="">
+            <h5>{project.title}</h5>
+            <p>{project.description}</p>
+        </article>
+    {/each}
 </div>
+
 
 <style>
 .projects {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(20em, 1fr)); /* Aumentando o valor mínimo de 15em para 20em */
-    min-width: 20em;  /* Ajuste o valor mínimo */
-    max-width: 1fr;
+    grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
+    /* grid-template-rows: masonry; */
+    margin: 1cm 0 1cm 0;
+    grid-gap: 10px;
 }
 
 article {
     margin: 10px;
-    display: grid;
-    grid-template-rows: auto auto 1fr;
 }
 
 article img {
@@ -69,10 +57,11 @@ article h5 {
 }
 
 article p {
+    position: relative;
     max-height: 0;
     opacity: 0;
     overflow: hidden;
-    transition: max-height 0.3s ease, opacity 0.3s ease;
+    transition: max-height 0.3s ease-in-out, opacity 0.3s ease;
     padding: 0 10px;
 }
 
@@ -81,6 +70,7 @@ article:hover p {
     max-height: 15rem;
     opacity: 1;
 }
+
 article:hover img {
     transform: scale(1.05);
     transition: transform 0.3s ease-in-out;
