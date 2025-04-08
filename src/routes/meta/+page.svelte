c763dcb0877b6a5bcf844e066c79a171a1dda15d
Vitor do Nascimento <vitornascimento@proton.me>
2025-04-08 13:13:49 -0300

    fix csv

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -31,7 +31,7 @@ usableArea.height = usableArea.bottom - usableArea.top;
 
 
 onMount(async () => {
-    data = await d3.csv("/loc.csv", row => ({
+    data = await d3.csv("./loc.csv", row => ({
         ...row,
         line: Number(row.line), // or just +row.line
         depth: Number(row.depth),
